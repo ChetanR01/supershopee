1fecc4f03cb9deb28d1d9dd95cea12c281477a84
ChetanR01 <chetan0101career@gmail.com>
2023-01-31 07:34:59 +0530

    Orders: view all orders, view detail&Update order states,price invoice

--- admin_dashboard/urls.py
+++ admin_dashboard/urls.py
@@ -11,9 +11,12 @@ urlpatterns = [
     # re_path(r'^getSubcategory/$', views.get_subcategory),
     path('',views.index, name="index"),
     path('products/',views.product, name="products"),
+    path('orders/',views.orders, name="orders"),
+    path('create-order/',views.create_order, name="create-order"),
+    path('update-order/<str:id>',views.update_order, name="update-order"),
     path('accounts/',views.account, name="accounts"),
     path('login/',views.login, name="login"),
-    path('add-product/',views.add_product, name="add-product"),
+    path('add-product/<int:id>',views.add_product, name="add-product"),
     path('edit-product/<int:id>',views.edit_product, name="edit-product"),
     path('add-category/',views.add_category, name="add-category"),
     
--- admin_dashboard/views.py
+++ admin_dashboard/views.py
@@ -7,10 +7,40 @@ def index(request):
     orders = Order.objects.all().order_by("-date")
     return render(request, "01-index.html", {"orders":orders})
 
+def orders(request):
+    orders = Order.objects.all().order_by("-date")
+    return render(request, "01-orders.html", {"orders":orders,})
+
+def create_order(request):
+    orders = Order.objects.all().order_by("-date")
+    return render(request, "01-create-order.html", {"orders":orders,})
+
+def update_order(request,id):
+    orders = Order.objects.filter(order_id=id)
+
+    if request.method=="POST":
+        order_status = request.POST.get('order_status')
+        payment_status = request.POST.get('payment_status')
+
+        for order in orders:
+            order.order_status=order_status
+            order.payment_status=payment_status
+            order.save()
+        return redirect("/dashboard/orders")
+
+    grand_total=0
+    for order in orders:
+        rep_order=order
+        grand_total+=order.total_cost
+    return render(request, "01-update-order.html", {"orders":orders,"rep_order":rep_order,"grand_total":grand_total})
+
 def product(request):
     products = ProductDetails.objects.all()
+    raw_id=1
+    for product in products:
+        raw_id = product.id
     categories = Category.objects.all()
-    return render(request, "01-products.html", {"products":products,"categories":categories})
+    return render(request, "01-products.html", {"products":products,"categories":categories,"raw_id":raw_id})
 
 def account(request):
     products = ProductDetails.objects.all()
@@ -21,9 +51,9 @@ def login(request):
     return render(request, "01-login.html", {})
 
 # Add new product
-def add_product(request):
+def add_product(request,id):
     if request.method == "POST":
-        form = ProductForm(request.POST or None, request.FILES or None)
+        form = ProductForm(id,request.POST or None, request.FILES or None)
         if form.is_valid():
             category_id = request.POST.get('category')
             subcategory_id = request.POST.get('subcategory')
@@ -35,9 +65,9 @@ def add_product(request):
             return redirect("/dashboard/products")
         else:
             print("ERROR !!! Unable to validate form ")
-            return render("/dashboard/add-product")
+            return render(f"/dashboard/add-product/{id}")
 
-    form = ProductForm()
+    form = ProductForm(p_id=id)
     categories=Category.objects.all()
     subcategories=SubCategory.objects.all()
     return render(request, "01-add-product.html", {"form":form,"categories":categories,"subcategories":subcategories})
@@ -60,7 +90,7 @@ def edit_product(request,id):
             return redirect("/dashboard/products")
         else:
             print("ERROR !!! Unable to validate form ")
-            return redirect("/dashboard/add-product")
+            return redirect(f"/dashboard/edit-product/{id}")
 
     form = ProductForm(p_id=id)
     print("Form Data = ",form.product.name)
